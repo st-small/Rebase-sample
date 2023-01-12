02e9bf6c4d037a7b386729ebf07a4dd9e6c2c649
Stanly Shianovskiy <st-small@rambler.ru>
2023-01-12 08:48:38 +0200

    First main view commit

---
+++ Rebase sample/Rebase sample/FirstMainView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstMainView.swift
+//  Rebase sample
+//
+//  Created by Stanly Shiyanovskiy on 12.01.2023.
+//
+
+import SwiftUI
+
+struct FirstMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirstMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstMainView()
+    }
+}
