4e056287ea60a038386bfafbb820902e68f60dda
Stanly Shianovskiy <st-small@rambler.ru>
2023-01-12 08:46:51 +0200

    Project created

---
+++ Rebase sample/Rebase sample/Rebase_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rebase_sampleApp.swift
+//  Rebase sample
+//
+//  Created by Stanly Shiyanovskiy on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Rebase_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
